a7c85a72009604ee051675f035edeac622d8bb94
njiddasalifu <njiddasalifu@gmail.com>
2023-06-17 01:40:09 +0100

    completed header

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -3,16 +3,16 @@ import './header.css';
 
 const Header = () => {
   return (
-    <header>
+    <header className="header">
       <nav className="header-nav">
-        <ul>
+        <ul className="header-nav-list">
           <li><a href="/">Home</a></li>
           <li><a href="#Cars">Cars</a></li>
           <li><a href="#About">About</a></li>
-          <li><a href="#Contact Us">Contact Us</a></li>
+          <li><a href="#Contact">Contact Us</a></li>
         </ul>
       </nav>
-      <h1 className="header-title">Dream-Car</h1>
+      <h1 className="header-title">Dream-Carz</h1>
     </header>
   );
 };
